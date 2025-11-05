6f394ff23434959cbae9e0587aab9ed0d580ae6a
Abdulazeez Abu <brightdev10@outlook.com>
2025-11-05 13:24:31 +0100

    fixed config error in forked repo

--- stack2/main.tf
+++ stack2/main.tf
@@ -1,22 +1,41 @@
+terraform {
+  cloud {
+    organization = "studio-luxe"
+
+    workspaces {
+      name = "aws-bedrock-project-stack2"
+    }
+  }
+}
+
 provider "aws" {
-  region = "us-west-2"  
+  region = "us-west-2"
+}
+
+data "terraform_remote_state" "stack1" {
+  backend = "remote"
+
+  workspace {
+    organization = "studio-luxe"
+    name         = "aws-bedrock-project"
+  }
 }
 
 module "bedrock_kb" {
-  source = "../modules/bedrock_kb" 
+  source = "../modules/bedrock_kb"
 
   knowledge_base_name        = "my-bedrock-kb"
   knowledge_base_description = "Knowledge base connected to Aurora Serverless database"
 
-  aurora_arn        = "" #TODO Update with output from stack1
+  aurora_arn        = data.terraform_remote_state.stack1.outputs.aurora_arn
   aurora_db_name    = "myapp"
-  aurora_endpoint   = "" # TODO Update with output from stack1
+  aurora_endpoint   = data.terraform_remote_state.stack1.outputs.aurora_endpoint
   aurora_table_name = "bedrock_integration.bedrock_kb"
   aurora_primary_key_field = "id"
   aurora_metadata_field = "metadata"
   aurora_text_field = "chunks"
   aurora_verctor_field = "embedding"
   aurora_username   = "dbadmin"
-  aurora_secret_arn = "" #TODO Update with output from stack1
-  s3_bucket_arn = "" #TODO Update with output from stack1
+  aurora_secret_arn = data.terraform_remote_state.stack1.outputs.rds_secret_arn
+  s3_bucket_arn = data.terraform_remote_state.stack1.outputs.s3_bucket_name
 }
